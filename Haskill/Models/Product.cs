06477da8ed7d853b6883c17b981a59f9ade71235
KaloyanGPetrov <138588760+KaloyanGPetrov@users.noreply.github.com>
2024-11-30 13:04:26 +0200

    update1

---
+++ Haskill/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Haskill.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string UnitOfMeasurments { get; set; }
+        public int Waste { get; set; }
+        public int Sold { get; set; }
+        public int Bought { get; set; }
+    }
+}
